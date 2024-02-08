ab2bbda50f53e218d1afc87c4c178ec0a46fa79f
Nishant Saini <ns0109375@gmail.com>
2024-02-08 14:12:49 +0530

    zod

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const zod = require('zod');
 const app = express();
 const port = 3000;
 
-const schema = zod.
+const schema = zod.array(zod.number());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+    try {
+        const data = schema.parse(req.body);
+        console.log(data);
+        res.send('Data is valid');
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({
+            error: error.errors
+        });
     }
-);
 
+    })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
