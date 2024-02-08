407a8e8493adb0d59019825f56f238feab8aa67c
Nishant Saini <ns0109375@gmail.com>
2024-02-08 15:21:45 +0530

    zod

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const zod = require('zod');
 const app = express();
 const port = 3000;
 
-const schema = zod.array(zod.number());
+const schema = zod.object({
+    email:zod.string().email(),
+    password:zod.string().min(6),
+})
 
 app.use(express.json());
 
